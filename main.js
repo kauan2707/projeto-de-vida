47f8741d1c9640d4aa5e3786ec13bec094c15002
kauan2707 <kauan.orias@escola.pr.gov.br>
2026-04-27 10:22:51 -0300

    olk1

---
+++ main.js
@@ -0,0 +1,5 @@
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0; i <botoes.length; i++){
+    botoes[i].onlick = function (){
+        for(let j=this.onfullscreenchange; j <botoes.length; j++)}}
